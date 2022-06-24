e4490b66b7d1f10d2caab06e81519b0c86f863b3
JuanManu59 <juan5.9@hotmail.com>
2022-06-24 16:52:51 -0500

    Ajustes generales

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -4,63 +4,62 @@ const { StatusCodes } = require('http-status-codes');
 const md5 = require('md5');
 
 const path = 'https://api.github.com/users/aperdomob';
-const path2 = 'https://api.github.com/repos/aperdomob';
 
-const query1 = {
+const user = {
   name: 'Alejandro Perdomo',
   company: 'Perficient Latam',
   location: 'Colombia'
 };
 
-const query2 = {
+const jasmineRepository = {
   full_name: 'aperdomob/jasmine-json-report',
   private: false,
   description: 'A Simple Jasmine JSON Report'
 };
 
-const query3 = {
+const readme = {
   name: 'README.md',
   path: 'README.md',
   sha: '360eee6c223cee31e2a59632a2bb9e710a52cdc0'
 };
 
-let result = null;
+let jasmineReportRepository = null;
 
-describe('Metodos GET', () => {
-  it('Verficacion del usuario', async () => {
-    const response = await axios.get(`${path}`);
+describe('GET methods', () => {
+  it('User verification', async () => {
+    const serviceHypermedia = await axios.get(`${path}`);
 
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data.name).to.equal(query1.name);
-    expect(response.data.company).to.equal(query1.company);
-    expect(response.data.location).to.equal(query1.location);
+    expect(serviceHypermedia.status).to.equal(StatusCodes.OK);
+    expect(serviceHypermedia.data.name).to.equal(user.name);
+    expect(serviceHypermedia.data.company).to.equal(user.company);
+    expect(serviceHypermedia.data.location).to.equal(user.location);
   });
 
-  it('Verficacion del contenido del repositorio', async () => {
-    const response = await axios.get(`${path}/repos`);
-    const repos = response.data;
-    result = repos.find(({ name }) => name === 'jasmine-json-report');
+  it('Repository Content Verification', async () => {
+    const repositories = await axios.get(`${path}/repos`);
+    const repositoriesData = repositories.data;
+    jasmineReportRepository = repositoriesData.find(({ name }) => name === 'jasmine-json-report');
 
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(result.full_name).to.equal(query2.full_name);
-    expect(result.private).to.equal(query2.private);
-    expect(result.description).to.equal(query2.description);
+    expect(repositories.status).to.equal(StatusCodes.OK);
+    expect(jasmineReportRepository.full_name).to.equal(jasmineRepository.full_name);
+    expect(jasmineReportRepository.private).to.equal(jasmineRepository.private);
+    expect(jasmineReportRepository.description).to.equal(jasmineRepository.description);
   });
 
-  it('Verfica que descargue correctamente el repositorio', async () => {
-    const descargaJasmine = await axios.get(`${result.html_url}/archive/refs/heads/${result.default_branch}/.zip`);
-    const md5Descarga = md5(descargaJasmine.data);
+  it('Verify repository download', async () => {
+    const downloadedRepo = await axios.get(`${jasmineReportRepository.html_url}/archive/refs/heads/${jasmineReportRepository.default_branch}/.zip`);
+    const md5downloaded = md5(downloadedRepo.data);
 
-    expect(descargaJasmine.status).to.equal(StatusCodes.OK);
-    expect(md5Descarga).to.equal('3876dfb1a98166adfcd5829cc4b3d156');
+    expect(downloadedRepo.status).to.equal(StatusCodes.OK);
+    expect(md5downloaded).to.equal('3876dfb1a98166adfcd5829cc4b3d156');
   });
 
-  it('Verifica el contenido del README.md', async () => {
-    const contenidoRepo = await axios.get(`${path2}/jasmine-json-report/contents/`);
-    const readme = contenidoRepo.data.find(({ name }) => name === 'README.md');
+  it('Verify the content of the README.md', async () => {
+    const repositoryContent = await axios.get(`${jasmineReportRepository.url}/contents/`);
+    const readmeData = repositoryContent.data.find(({ name }) => name === 'README.md');
 
-    expect(readme.name).to.equal(query3.name);
-    expect(readme.path).to.equal(query3.path);
-    expect(readme.sha).to.equal(query3.sha);
+    expect(readmeData.name).to.equal(readme.name);
+    expect(readmeData.path).to.equal(readme.path);
+    expect(readmeData.sha).to.equal(readme.sha);
   });
 });
