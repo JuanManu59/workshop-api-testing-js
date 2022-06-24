c7d37a8c96941f23b565e045733ddccf22077294
Juan Manuel López Cleves <33491290+JuanManu59@users.noreply.github.com>
2022-06-23 21:04:30 -0500

    Errores codigo estatico (#5)
    * Errores codigo estatico
    
    * fix my test
    
    * EOF eslint
    
    * EOF eslintv2
    
    * fix dev dependencies

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -5,53 +5,53 @@ const { StatusCodes } = require('http-status-codes');
 const path = 'https://httpbin.org';
 
 const query = {
-    name: 'Carlo',
-    age: '19',
-    city: 'New York'
+  name: 'Carlo',
+  age: '19',
+  city: 'New York'
 };
 
 describe('First Api Tests', () => {
-    it('Consume GET Service', async () => {
-        const response = await axios.get(`${path}/ip`);
+  it('Consume GET Service', async () => {
+    const response = await axios.get(`${path}/ip`);
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data).to.have.property('origin');
-    });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property('origin');
+  });
 
-    it('Consume GET Service with query parameters', async () => {
-        const response = await axios.get(`${path}/get`, { query });
+  it('Consume GET Service with query parameters', async () => {
+    const response = await axios.get(`${path}/get`, { query });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.query).to.eql(query);
-    });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.query).to.eql(query);
+  });
 
-    it('Consume HEADER Service', async () => {
-        const response = await axios.head(`${path}/headers`);
-    
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.headers).to.have.property('content-type');
-        expect(response.headers).to.have.property('content-length');
-        expect(response.data).to.eql('');
-    });
+  it('Consume HEADER Service', async () => {
+    const response = await axios.head(`${path}/headers`);
 
-    it('Consume PATCH Service', async () => {
-        const response = await axios.patch(`${path}/patch`, query );
-    
-        expect(response.status).to.equal(StatusCodes.OK);   
-        expect(response.data.json).to.eql(query);
-    });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.headers).to.have.property('content-type');
+    expect(response.headers).to.have.property('content-length');
+    expect(response.data).to.eql('');
+  });
 
-    it('Consume PUT Service', async () => {
-        const response = await axios.put(`${path}/put`,  query );
-    
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.equal(JSON.stringify(query));
-    });
+  it('Consume PATCH Service', async () => {
+    const response = await axios.patch(`${path}/patch`, query);
 
-    it('Consume DELETE Service', async () => {
-        const response = await axios.delete(`${path}/delete`);
-    
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data.data).to.eql('');
-    });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.json).to.eql(query);
+  });
+
+  it('Consume PUT Service', async () => {
+    const response = await axios.put(`${path}/put`, query);
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.data).to.equal(JSON.stringify(query));
+  });
+
+  it('Consume DELETE Service', async () => {
+    const response = await axios.delete(`${path}/delete`);
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data.data).to.eql('');
+  });
 });
