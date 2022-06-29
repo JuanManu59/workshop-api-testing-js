913db2e73736dca5abc3f3b34aba631609c96cf0
JuanManu59 <juan5.9@hotmail.com>
2022-06-29 12:11:35 -0500

    Cambios solicitados

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -2,11 +2,12 @@ const axios = require('axios');
 const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
-const path = 'https://api.github.com/user/following/aperdomob';
+const path = 'https://api.github.com/user/following';
 const token = process.env.ACCESS_TOKEN;
 
 let response;
 let followingList;
+let userFind;
 
 const instance = axios.create({
   baseURL: path,
@@ -15,7 +16,7 @@ const instance = axios.create({
 
 describe('PUT methods', () => {
   before(async () => {
-    response = await instance.put(`${path}`);
+    response = await instance.put(`${path}/aperdomob`);
   });
 
   it('Follow a user service', () => {
@@ -25,24 +26,36 @@ describe('PUT methods', () => {
 
   describe('Verify following List', () => {
     before(async () => {
-      const result = path.replace('/aperdomob', '');
-      followingList = await instance.get(`${result}`);
+      followingList = await instance.get(`${path}`);
+      userFind = followingList.data.find(({ login }) => login === 'aperdomob');
     });
 
     it('Method to get list of followed', () => {
       expect(followingList.status).to.equal(StatusCodes.OK);
-      expect(followingList.data[0].login).to.equal('aperdomob');
+      expect(userFind.login).to.equal('aperdomob');
     });
   });
 
   describe('Verify the idempotence of the method', () => {
     before(async () => {
-      response = await instance.put(`${path}`);
+      response = await instance.put(`${path}/aperdomob`);
     });
 
     it('follow a user a second time', () => {
       expect(response.status).to.equal(StatusCodes.NO_CONTENT);
       expect(response.data).to.equal('');
+    });
+
+    describe('Verify that continued to follow the user', () => {
+      before(async () => {
+        followingList = await instance.get(`${path}`);
+        userFind = followingList.data.find(({ login }) => login === 'aperdomob');
+      });
+
+      it('Verify that follow the indicated user', () => {
+        expect(followingList.status).to.equal(StatusCodes.OK);
+        expect(userFind.login).to.equal('aperdomob');
+      });
     });
   });
 });
