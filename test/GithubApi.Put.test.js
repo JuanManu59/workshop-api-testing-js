5c8282ab5101ed0fb2d29fe249e294284b219217
JuanManu59 <juan5.9@hotmail.com>
2022-06-29 09:57:56 -0500

    Consumiento metodos put

---
+++ test/GithubApi.Put.test.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+const path = 'https://api.github.com/user/following/aperdomob';
+const token = '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';
+
+let response;
+let followingList;
+
+const instance = axios.create({
+  baseURL: path,
+  headers: { Authorization: `token ${token}` }
+});
+
+describe('PUT methods', () => {
+  before(async () => {
+    response = await instance.put(`${path}`);
+  });
+
+  it('Follow a user service', () => {
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    expect(response.data).to.equal('');
+  });
+
+  describe('Verify following List', () => {
+    before(async () => {
+      const result = path.replace('/aperdomob', '');
+      followingList = await instance.get(`${result}`);
+    });
+
+    it('Method to get list of followed', () => {
+      expect(followingList.status).to.equal(StatusCodes.OK);
+      expect(followingList.data[0].login).to.equal('aperdomob');
+    });
+  });
+
+  describe('Verify the idempotence of the method', () => {
+    before(async () => {
+      response = await instance.put(`${path}`);
+    });
+
+    it('follow a user a second time', () => {
+      expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+      expect(response.data).to.equal('');
+    });
+  });
+});
