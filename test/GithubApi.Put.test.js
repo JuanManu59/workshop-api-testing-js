59f2927de13ba49b94372314c9e66730c07e4f75
JuanManu59 <juan5.9@hotmail.com>
2022-06-29 10:43:54 -0500

    Ajuste v1

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
 const path = 'https://api.github.com/user/following/aperdomob';
-const token = process.env.ACCESS_TOKEN;
+const token = '';
 
 let response;
 let followingList;
