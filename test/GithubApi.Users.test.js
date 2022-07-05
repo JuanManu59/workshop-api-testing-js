f31ebb56ec4b80ce8fe496e814f77c3f38665a47
Juan Manuel López Cleves <33491290+JuanManu59@users.noreply.github.com>
2022-07-05 10:14:07 -0500

    test query params v1 (#49)
    * test query params v1
    
    * test users v2

---
+++ test/GithubApi.Users.test.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const { expect } = require('chai');
+const { StatusCodes } = require('http-status-codes');
+
+const path = 'https://api.github.com';
+
+let response;
+
+describe('Github user list', () => {
+  describe('Service to find all users', () => {
+    before(async () => {
+      response = await axios.get(`${path}/users`);
+    });
+
+    it('should contain 30 users by default paging', () => {
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.length).to.equal(30);
+    });
+  });
+
+  describe('Service to find 10 users', () => {
+    before(async () => {
+      response = await axios.get(`${path}/users`, { params: { per_page: 10 } });
+    });
+
+    it('the size of the list of users is 10', () => {
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.length).to.equal(10);
+    });
+  });
+
+  describe('Service to find 100 users', () => {
+    before(async () => {
+      response = await axios.get(`${path}/users`, { params: { per_page: 100 } });
+    });
+
+    it('the size of the list of users is 100', () => {
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.length).to.equal(100);
+    });
+  });
+});
